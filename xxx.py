947ba50d7b3eb0085d6e2b43fba5ae2b0bd9fca8
тєαм ѕтαя <115808731+TEAMSTARWORLD@users.noreply.github.com>
2022-11-13 10:39:05 +0530

    Delete xxx.py

--- xxx.py
+++
@@ -1,84 +0,0 @@
-o
-
-|�sb�+�@s�dZddlmZddlmZddlmZmZmZm	Z	ddlm
-Z
-mZmZm
-Z
-ddlmZddlZddlZddlZddlmZmZdd	lmZdd
-lmZddlZddlZddlmZmZddlZddlZe��Z e �!d�e d
-dZ"e d
-dZ#e$e"�Z%e&e#�Z'e�ej(Z)ej*Z+ej,Z-ej.Z/dZ0ej1Z2ej3Z4e)e+e/e4e2gZ5e+de/de+de-Z6e+de)de+de-Z7e/de+de/de-Z8e+de2de+de-Z9e/de+de/de-Z:e/de+de/de-Z;dd�Z<dd�Z=gZ>e?dd�Z@	z
-e>�Ae�Be@��Wn
-eC�yYnw�qeDd e6e+d!e-�e>D]rZEeEdZFeDe:�e0�d"e+�eF���ed#eF��e%e'�ZGeG�H�gZIeG�J��s{zeG�KeF�eDd$�Wn e�yzeDe7�d%e/�eF�d%e)�d&e-���eI�AeE�YnweID]ZLe>�MeL�eDe6e+d'e-��q}e�Nd(�eG�O��q(eDe6d)�e=�e<�d*d+�ZPd,d-�ZQzce?d.d��SZ@e�Be@�ZRe@�S�eTe9�e2�d/e/�eRd�e+�d0e)���ZUd1eUv�r�eRdZVe$eRd2�ZWnejXd3k�r�e�Yd4�ne�Yd5�eTe9�e2�d6e)���ZVdZWWd�n	1�swYWneTe9�e2�d6e)���ZVdZWYgZ>e?dd�Z@	z
-e>�Ae�Be@��Wn
-eC�y@Ynw�q,eDe6�e+�d7e/�eZe>����e$eTe9�e2�d8e)����Z[eDe6�e2�d9e+���eDe2�d:e+�d;��eDe2�d<e+�d=��e$eTe9�e2�d>e)����Z\e\dk�r�e&eTe9�e2�d?e)����Z]n
-e&eTe9�e2�d@e)����Z]eDe0�dA�dB�dCdD�e>de[�D�Z^e^D]Z_e>�Me_��q�e?ddE��$Z@e>D]	ZEe�`eEe@��q�e^D]	Zae�`eae@��q�e@�S�Wd�n	1�s�wYe$eTe9�e2�dFe/�de+�dGe/�dHe)��
-��ZbeDe8�e+�dIe/�eZe^��e+�dJ��dZcdZde^D�]ZeeWdKZfed#eed��e%e'�ZgeDe:�e0�dLe2�eed�e+�dMe2�dN�	�eg�heed�eg�i�jjZkz�dOeVv�r�eV�ldO�d2Zmzegeem��eDe:�e0�dLe2�ek�e+�dP��Wn e�y�YnwegeeV��eDe:�e0�dLe2�ek�e+�dP��eg�neV�Zoe\dk�r�egee]��eDe:�e0�dLe2�ek�e+�dQ��eg�ne]�Zpeepjqepjr�Zsn1ze]�ldO�d2Ztegeet��eDe:�e0�dLe2�ek�e+�dQ��Wn
-e�y�Ynweg�ne]�ZpepZsWn/eu�y1Zvz"eDe7�e)�dLe2�ek�e+�dR��eDe7�d%e)�ev���WYdZv[v�q*dZv[vwweDe:�e0�dLe2�ek�e+�dMe2�dS�	�zgZwegjxeodTdU�ZwWn(eu�yxZvzeDe7�e)�dV��eDe7�e)�d%ev���WYdZv[v�q*dZv[vwweZew�Zdeddk�s�J�eWedk�r�eDe7�e+�dW���q*eDe6�e+�dXe/�eW���dZyeweWef�D�]�ZzeWd27ZWeydYk�r�eDe7�e)�dZ���n�z]e\dk�r�egeesezg��n	egeesjqezd[��ezjjZ{epj|Z}eDe:�e0�dLe2�ek�e+�dMe2�e{�d%e+�d\e2�e}���ecd27ZceDe6�e0�dLe2�ek�e+�d]e/�eb�d%e+�d^��e�Neb�W�q�e�y<eDe;�e0�dLe2�ek�e+�dMe)�d_�	�Y�q�e�y]eDe7�e0�dLe2�ek�e+�dMe)�d`�	�eyd27ZyY�q�e
-�y}eDe7�e)�da��eWedk�rwePeVeW�eQ�Y�q�e�y�eDe7�e0�dLe2�ek�e+�dMe)�db�	�Yn�e	�y�eDe7�e0�dLe2�ek�e+�dMe)�dc�	�Yn�e�y�eDe;�e0�dLe2�ek�e+�dMe)�dd�	�Y�q�e
-�y�ZvzeDe7�e)�d%ev���WYdZv[vnVdZv[vwe~�yeDe7�e)�de��Y�q�e�y'eDe7�e)�df��eWeZew�k�r!ePeVeW�eQ�Y�q�eu�yCZvzeDe7�d%ev���WYdZv[v�q�dZv[vww�q*ecdk�rUeDd e6�e+�dg��zeWedk�rfePeVeW�eQ�WdSWdSeQ�YdS)hz�
-=============SON OF GOD=====================
-REX 1.0 members adding script
-Coded by REX - @TG_STARWORLD
-Apologies if anything in the code is dumb :)
-Copy with credits
-************************************************
-�)�TelegramClient)�InputPeerChannel)�PeerFloodError�UserPrivacyRestrictedError�PhoneNumberBannedError�ChatAdminRequiredError)�ChatWriteForbiddenError�UserBannedInChannelError�UserAlreadyParticipantError�FloodWaitError)�InviteToChannelRequestN)�ImportChatInviteRequest�AddChatUserRequest)�JoinChannelRequest)�UserStatusRecently)�init�Forez
-setapi.iniZRex�input_api_id�input_api_hashz[97m�[�i�]�!�*�~�+�-cCsVgd�}|D]}tt�t��|�t���qtt�dt�dt�dt�dt�d�
-�dS)N)uN╔════╗────╔╗─────╔═══╦╗─╔╗uN║╔╗╔╗║────║║─────║╔═╗║║─║║uW╚╝║║╠╩╦╦══╣║╔╦╗─╔╣║─║║╚═╣╚═╦╗uW──║║║╔╬╣╔═╣╚╝╣║─║║╚═╝║╔╗║╔╗╠╣uW──║║║║║║╚═╣╔╗╣╚═╝║╔═╗║╚╝║║║║║uW──╚╝╚╝╚╩══╩╝╚╩═╗╔╩╝─╚╩══╩╝╚╩╝u3─────────────╔═╝║u3─────────────╚══╝z   Version: z2.0z | Author: Z
-StarWorld�
-)�print�random�choice�colors�rs�lg�w)�b�char�r'�/storage/emulated/0/nt/xxx.py�banner7s
-*r)cCs&tjdkrt�d�dSt�d�dS)N�nt�cls�clear)�os�name�systemr'r'r'r(�clrJs
-r0zvars.txt�rbTrz  Checking for banned accounts...z
- Checking z	sessions/ZOK� z
-is banned!z< Banned account removed[Remove permanently using manager.py]g�?z Sessions created!cCsftdd��}t�|t|�g|�|��Wd�n1swYtt�t�dt�dt���dS)N�
-status.dat�wbz Session stored in )	�open�pickle�dump�int�closer�infor#r$)Zscraped�index�fr'r'r(�
-log_statusts
-
-� r=cCs(tdt�d��t�t�t��dS)Nrz Press enter to exit...)�input�cyr0r)�sys�exitr'r'r'r(�exit_window{srBr3z Resume scraping members from z	? [y/n]: �y�r*zdel status.datz
-rm status.datz. Public/Private group link to scrape members: z Total accounts: z" Enter number of accounts to use: z Choose an optionz[0]z Add to public groupz[1]z Add to private groupz Enter choice: z Enter public group link: z Enter private group link: �_�2cCsg|]}|�qSr'r')�.0�xr'r'r(�
-<listcomp>�srIr4z Enter delay time per requestz
-0 for Nonez]: z -- Adding members from z account(s) --�<z User: z -- zStarting session... z
-/joinchat/z -- Joined group to scrapez -- Joined group to addz -- Failed to join groupzRetrieving entities...i|)�limitz Couldn't scrape membersz No members to add!z Start: �
-z/ Too many Peer Flood Errors! Closing session...�*z--> z
- -- Sleep z	second(s)zUser Privacy Restricted ErrorzPeer Flood Error.zA Can't add to group. Contact group admin to enable members addingzBanned from writing in groupszChat Admin rights needed to addzUser is already a participantz Error in Entityz ---- Adding Terminated ----z Adding session ended)��__doc__Z
-telethon.syncrZtelethon.tl.typesrZtelethon.errors.rpcerrorlistrrrrrr	r
-rZtelethon.tl.functions.channelsrr@ZcsvZconfigparserZtelethon.tl.functions.messagesr
-rrr�timerZcoloramarrr-r6ZConfigParserZconfig�readrrr8Zapi_id�strZapi_hashZRED�rZGREENr#ZRESETr"ZWHITEr$ZgreyZCYANr?ZYELLOWZyer!r:�error�successZINPUTZplusZminusr)r0Zaccountsr5r<�append�load�EOFErrorr�aZphnZclntZconnectZbannedZis_user_authorizedZsend_code_request�z�remove�sleepZ
-disconnectr=rB�statusr9r>ZlolZscraped_grpr;r.r/�lenZnumber_of_accsr �targetZto_use�lr7�abZ
-sleep_timeZ
-adding_statusZapprox_members_countZacc�stop�c�startZget_meZ
-first_nameZacc_name�splitZg_hashZ
-get_entityZscraped_grp_entityZ
-target_entity�idZaccess_hashZtarget_detailsZgrp_hash�	Exception�e�membersZget_participantsZpeer_flood_status�userZuser_id�titleZtarget_title�
-ValueError�KeyboardInterruptr'r'r'r(�<module>s�
-
-
-��
-
-"�
-
-
-(
-
-���
-
-��
-
-�,$
-*
-
-$� 
-
- 
-$�
-� ��&��
-
-
-80&&
-
-&&&�
-���
-
-
-�
